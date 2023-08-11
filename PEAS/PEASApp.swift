d8e35d2f1fd0eae46a5d86055efad1581fb6af3b
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-11 01:08:04 -0700

    Setup Project

---
+++ PEAS/PEASApp.swift
@@ -0,0 +1,17 @@
+//
+//  PEASApp.swift
+//  PEAS
+//
+//  Created by Kingsley Okeke on 2023-08-11.
+//
+
+import SwiftUI
+
+@main
+struct PEASApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
