7ce09ef9e1fdeae28c9bdf3491800f50652f5121
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-11 20:48:18 -0700

    Add CustomersView Add CustomersVM

---
+++ PEAS/View/Customers/CustomersVM.swift
@@ -0,0 +1,14 @@
+//
+//  CustomersVM.swift
+//  PEAS
+//
+//  Created by Kingsley Okeke on 2023-08-11.
+//
+
+import Foundation
+
+extension CustomersView {
+	@MainActor class ViewModel: ObservableObject {
+		
+	}
+}
---
+++ PEAS/View/Customers/CustomersView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomersView.swift
+//  PEAS
+//
+//  Created by Kingsley Okeke on 2023-08-11.
+//
+
+import SwiftUI
+
+struct CustomersView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct CustomersView_Previews: PreviewProvider {
+	static var previews: some View {
+		CustomersView()
+	}
+}
