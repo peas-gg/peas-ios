0c143f4e1f48d9688bf658b6a85829bb57ae5d61
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-12 21:17:05 -0700

    Display months in the Calendar View

--- PEAS/View/Calendar/CalendarView.swift
+++ PEAS/View/Calendar/CalendarView.swift
@@ -10,8 +10,39 @@ import SwiftUI
 struct CalendarView: View {
 	@StateObject var viewModel: ViewModel
 	
+	let months: [Date] = CalendarClient.shared.months
+	
 	var body: some View {
-		Text("Hello, World!")
+		VerticalTabView {
+			ForEach(0..<months.count, id: \.self) {
+				monthView(currentIndex: $0)
+			}
+		}
+		.background(Color.accentColor)
+		.tabViewStyle(.page(indexDisplayMode: .never))
+	}
+	
+	@ViewBuilder
+	func monthView(currentIndex: Int) -> some View {
+		Group {
+			let count = months.count
+			let nextIndex = currentIndex + 1
+			if count == 1 {
+				MonthView(month: months[currentIndex])
+			} else if currentIndex % 2 == 0, nextIndex < count {
+				VStack {
+					MonthView(month: months[currentIndex])
+					Spacer()
+					MonthView(month: months[nextIndex])
+					Spacer()
+				}
+			} else if currentIndex % 2 == 0, nextIndex >= count {
+				VStack {
+					MonthView(month: months[currentIndex])
+					Spacer()
+				}
+			}
+		}
 	}
 }
 
