dea7d51f31c755068e72a28e17d5ab49802d90ed
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-18 17:19:55 -0700

    Add HTTPMethod Add APPError Add ResponseTypes Add URLSession+Validation

---
+++ PEAS/Model/AppError.swift
@@ -0,0 +1,20 @@
+//
+//  AppError.swift
+//  PEAS
+//
+//  Created by Kingsley Okeke on 2023-08-18.
+//
+
+import Foundation
+
+enum AppError: Error {
+	enum APIClientError: Error, Equatable {
+		case authExpired
+		case invalidURL
+		case httpError(statusCode: Int, data: Data)
+		case decodingError
+		case rawError(String)
+		
+		var title: String { "API Error" }
+	}
+}
