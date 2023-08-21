f33fd2b7ea15ac65ae3c9666e9a3caa314b66546
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-20 18:42:23 -0700

    Use ScrollView instead List

--- PEAS/View/Dashboard/DashboardView.swift
+++ PEAS/View/Dashboard/DashboardView.swift
@@ -9,111 +9,127 @@ import SwiftUI
 
 struct DashboardView: View {
 	@StateObject var viewModel: ViewModel
-    
-    struct Service: Identifiable {
-        let id = UUID()
-        let imageUrl: String
-        let name: String
-        let price: String
-        let status: String
-        
-    }
-    
-    let url = URL(string: "https://en.gravatar.com/userimage/238873705/657a6a16307fd0a8c47e1d7792173277.jpeg?size=256")
-    
-    let services: [Service] = [
-        Service(imageUrl:"https://en.gravatar.com/userimage/238873705/657a6a16307fd0a8c47e1d7792173277.jpeg?size=256", name: "Sweet cut", price: "$300", status: "Pending"),
-        Service(imageUrl:"https://en.gravatar.com/userimage/238873705/161389d4185bdba59229b83175ac5cb0.jpeg?size=256", name: "Fire fist", price: "$250", status: "Pending"),
-           // Service(name: "Service 2", status: "Approved"),
-            //Service(name: "Service 3", status: "Completed")
-        ]
-    
-	var body: some View {
-        VStack {
-            HStack {
-                VStack (alignment: .leading){
-                    Text("Welcome,")
-                        .foregroundColor(Color.app.tertiaryText)
-                    Text("Melissa")
-                        .foregroundColor(Color.app.primaryText)
-                }
-                .font(.system(size: FontSizes.title1, weight: .semibold, design: .rounded))
-                .padding()
-                Spacer()
-                
-                AsyncImage(url: url) { phase in
-                    switch phase {
-                    case .empty:
-                        ProgressView()
-                    case .success(let returnImage):
-                        returnImage
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 60)
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
-                            //cornerRadius(30)
-                    case .failure:
-                        Image(systemName: "questionmark")
-                            .font(.headline)
-                    default:
-                        Image(systemName: "questionmark")
-                            .font(.headline)
-                    }
-                }
-                .padding()
-                
-            }
-            .padding(.horizontal)
-            .padding(.top)
-            Spacer().frame(height: 30)
-            Text("$2,378.56")
-                .foregroundColor(.green)
-                .font(.system(size: 50, weight: .semibold, design: .rounded))
-            
-            List(services){
-                service in
-                HStack {
-                    HStack() {
-                        AsyncImage(url: URL(string: service.imageUrl)) { phase in
-                            switch phase {
-                            case .empty:
-                                ProgressView()
-                            case .success(let returnImage):
-                                returnImage
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 50, height: 60)
-                                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                                    //cornerRadius(30)
-                            case .failure:
-                                Image(systemName: "questionmark")
-                                    .font(.headline)
-                            default:
-                                Image(systemName: "questionmark")
-                                    .font(.headline)
-                            }
-                        }
-                        
-                        VStack(alignment: .leading){
-                            Text(service.name)
-                                .font(.headline)
-                            Text(service.price)
-                                .foregroundColor(Color.app.tertiaryText)
-                        }
-                        Spacer()
-                        Text(service.status)
-                            .font(.headline)
-                    }
-                }
-            }
-            
-            Spacer()
-        }
-        .foregroundColor(Color.app.primaryText)
-        .background(Color.app.primaryBackground)
-        
+	
+	struct Service: Identifiable {
+		let id = UUID()
+		let imageUrl: String
+		let name: String
+		let price: String
+		let status: String
+		
 	}
-    
+	
+	let url = URL(string: "https://en.gravatar.com/userimage/238873705/657a6a16307fd0a8c47e1d7792173277.jpeg?size=256")
+	
+	let services: [Service] = [
+		Service(imageUrl:"https://en.gravatar.com/userimage/238873705/657a6a16307fd0a8c47e1d7792173277.jpeg?size=256", name: "Sweet cut", price: "$300", status: "Pending"),
+		Service(imageUrl:"https://en.gravatar.com/userimage/238873705/161389d4185bdba59229b83175ac5cb0.jpeg?size=256", name: "Fire fist", price: "$250", status: "Pending"),
+		// Service(name: "Service 2", status: "Approved"),
+		//Service(name: "Service 3", status: "Completed")
+	]
+	
+	var body: some View {
+		VStack {
+			HStack {
+				VStack(alignment: .leading) {
+					Text("Welcome,")
+						.foregroundColor(Color.app.tertiaryText)
+					Text("Melissa")
+						.foregroundColor(Color.app.primaryText)
+				}
+				.font(.system(size: FontSizes.title1, weight: .semibold, design: .rounded))
+				.padding()
+				
+				Spacer()
+				
+				AsyncImage(url: url) { phase in
+					switch phase {
+					case .empty:
+						ProgressView()
+					case .success(let returnImage):
+						returnImage
+							.resizable()
+							.frame(width: 50, height: 60)
+							.scaledToFill()
+							.clipShape(RoundedRectangle(cornerRadius: 20))
+					case .failure:
+						Image(systemName: "questionmark")
+							.font(.headline)
+					default:
+						Image(systemName: "questionmark")
+							.font(.headline)
+					}
+				}
+				.padding()
+				
+			}
+			.padding(.horizontal)
+			.padding(.top)
+			
+			Spacer()
+				.frame(height: 30)
+			
+			Text("$2,378.56")
+				.foregroundColor(.green)
+				.font(.system(size: 50, weight: .semibold, design: .rounded))
+			
+			VStack {
+				ScrollView {
+					LazyVStack {
+						ForEach(services) { service in
+							HStack {
+								HStack() {
+									AsyncImage(url: URL(string: service.imageUrl)) { phase in
+										switch phase {
+										case .empty:
+											ProgressView()
+										case .success(let returnImage):
+											returnImage
+												.resizable()
+												.frame(width: 50, height: 60)
+												.scaledToFill()
+												.clipShape(RoundedRectangle(cornerRadius: 20))
+											//cornerRadius(30)
+										case .failure:
+											Image(systemName: "questionmark")
+												.font(.headline)
+										default:
+											Image(systemName: "questionmark")
+												.font(.headline)
+										}
+									}
+									
+									VStack(alignment: .leading){
+										Text(service.name)
+											.font(.headline)
+										Text(service.price)
+											.foregroundColor(Color.app.tertiaryText)
+									}
+									Spacer()
+									Text(service.status)
+										.font(.headline)
+								}
+							}
+						}
+					}
+				}
+				.padding(.top)
+				.padding(.horizontal, 30)
+			}
+			.background {
+				ZStack {
+					let cornerRadius: CGFloat = 10
+					RoundedRectangle(cornerRadius: cornerRadius)
+						.fill(Color.white)
+					RoundedRectangle(cornerRadius: cornerRadius)
+						.stroke(Color(uiColor: UIColor(hex: "E5E5E5")))
+				}
+				.padding(.horizontal, 10)
+			}
+		}
+		.foregroundColor(Color.app.primaryText)
+		.background(Color(uiColor: UIColor(hex: "F4F4F6")))
+	}
 }
 
 struct DashboardView_Previews: PreviewProvider {
