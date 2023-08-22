762480152c0273840bc874895af801629f769476
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-21 23:53:58 -0700

    Add TokenManager Add AppUrlRequest Add Authenticate Add User Update the api request to accept requests

--- PEAS/Clients/APIClient/APIClient.swift
+++ PEAS/Clients/APIClient/APIClient.swift
@@ -5,8 +5,65 @@
 //  Created by Kingsley Okeke on 2023-08-18.
 //
 
+import Combine
 import Foundation
+
+typealias APIClientError = AppError.APIClientError
 
 final class APIClient {
 	
+	static let shared: APIClient = APIClient()
+	
+	private var cancellables: Set<AnyCancellable> = Set<AnyCancellable>()
+	
+	let decoder: JSONDecoder = JSONDecoder()
+	
+	private func apiRequest<Output: Decodable>(appRequest: APPUrlRequest, output: Output.Type) -> AnyPublisher<Output, APIClientError> {
+		do {
+			return try urlRequest(urlRequest: appRequest.urlRequest)
+				.catch { error -> AnyPublisher<Data, Error> in
+					let failedPublisher: AnyPublisher<Data, Error> = Fail(error: error).eraseToAnyPublisher()
+					if let error  = error as? APIClientError {
+						if error == .authExpired {
+							return TokenManager.shared.refreshToken()
+								.flatMap { isSuccess -> AnyPublisher<Data, Error> in
+									let request = try! appRequest.urlRequest
+									if isSuccess {
+										return self.urlRequest(urlRequest: request)
+											.eraseToAnyPublisher()
+									} else {
+										return failedPublisher
+									}
+								}
+								.eraseToAnyPublisher()
+						} else {
+							return failedPublisher
+						}
+					} else {
+						return failedPublisher
+					}
+				}
+				.decode(type: output, decoder: self.decoder)
+				.mapError{ error in
+					if let error = error as? AppError.APIClientError {
+						return error
+					}
+					else {
+						return APIClientError.rawError(String(describing: error))
+					}
+				}
+				.eraseToAnyPublisher()
+		}
+		catch let error {
+			return Fail(error: APIClientError.rawError(String(describing: error)))
+					.eraseToAnyPublisher()
+		}
+	}
+	
+	func urlRequest(urlRequest: URLRequest) -> AnyPublisher<Data, Error> {
+		return URLSession.shared.dataTaskPublisher(for: urlRequest)
+			.validateStatusCode()
+			.map(\.data)
+			.eraseToAnyPublisher()
+	}
 }
---
+++ PEAS/Model/User.swift
@@ -0,0 +1,16 @@
+//
+//  User.swift
+//  PEAS
+//
+//  Created by Kingsley Okeke on 2023-08-21.
+//
+
+import Foundation
+
+struct User: Codable {
+	let firstName: String
+	let lastName: String
+	let email: String
+	let phone: String
+	let role: String
+}
