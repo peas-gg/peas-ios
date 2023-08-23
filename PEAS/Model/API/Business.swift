9d042533866b9261307f067ea6a9e5b2e305cfcc
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-08-22 22:25:19 -0700

    Add Block to Business

--- PEAS/Model/API/Business.swift
+++ PEAS/Model/API/Business.swift
@@ -8,6 +8,16 @@
 import Foundation
 
 struct Business: Codable {
+	struct Block: Codable {
+		let id: String
+		let blockType: String
+		let image: URL
+		let price: Double
+		let duration: Int
+		let title: String
+		let description: String
+	}
+	
 	let id: String
 	let sign: String
 	let name: String
@@ -21,6 +31,7 @@ struct Business: Codable {
 	let location: String
 	let timeZone: String
 	let isActive: Bool
+	let blocks: [Block]
 }
 
 extension Business {
@@ -38,7 +49,18 @@ extension Business {
 			tiktok: "",
 			location: "Vancouver, BC. Canada",
 			timeZone: "America/Los Angeles",
-			isActive: true
+			isActive: true,
+			blocks: [
+				Block(
+					id: "1",
+					blockType: "Genesis",
+					image: URL(string: "https://peasfilesdev.blob.core.windows.net/images/Jenny.jpg")!,
+					price: 40.0,
+					duration: 14000,
+					title: "I am just trying this out",
+					description: "This will take approximately 4 hours on average. But please add a note if you have any special request."
+				)
+			]
 		)
 	}()
 }
