35777511f8505f29681c43a4d6d720c1c179fbd0
Abdulsalam <Abello750@gmail.com>
2023-09-04 13:37:29 -0400

    Implementing the blend mode on the welcome page view

--- PEAS/View/Welcome/WelcomeView.swift
+++ PEAS/View/Welcome/WelcomeView.swift
@@ -11,9 +11,119 @@ struct WelcomeView: View {
 	@StateObject var viewModel: ViewModel
 	
 	var body: some View {
-		Button(action: { viewModel.startOnboarding() }) {
-			Text("Start")
+		
+		ZStack {
+			/*
+			AngularGradient(
+				gradient: Gradient(colors: [
+					Color(#colorLiteral(red: 0, green: 0.337254902, blue: 0.337254902, alpha: 1)),
+					Color(#colorLiteral(red: 0.368627451, green: 0.6509803922, blue: 0.8078431373, alpha: 1)),
+					Color(#colorLiteral(red: 0.3803921569, green: 0.8274509804, blue: 0.7450980392, alpha: 1)),
+					Color(#colorLiteral(red: 0.6470588235, green: 0.9411764706, blue: 0.9843137255, alpha: 1))
+				]),
+				center: .center)
+			
+			.ignoresSafeArea()
+			*/
+			
+			HStack{
+				VStack{
+					
+					LinearGradient(
+						gradient: Gradient(colors: [
+							Color(#colorLiteral(red: 0, green: 0.337254902, blue: 0.337254902, alpha: 1))
+							//Color(red: 0, green: 0.337254902, blue: 0.337254902),
+							//Color(red: 0.368627451, green: 0.6509803922, blue: 0.8078431373)
+						]),
+						startPoint: .topLeading,
+						endPoint: .bottomLeading
+					)
+					LinearGradient(
+						gradient: Gradient(colors: [
+							Color(#colorLiteral(red: 0.368627451, green: 0.6509803922, blue: 0.8078431373, alpha: 1))
+							//Color(red: 0.368627451, green: 0.6509803922, blue: 0.8078431373),
+							//Color(red: 0.6470588235, green: 0.9411764706, blue: 0.9843137255),
+							//Color(red: 0.3803921569, green: 0.8274509804, blue: 0.7450980392)
+						]),
+						startPoint: .topTrailing,
+						endPoint: .bottomTrailing
+					)
+					
+				}
+				
+				Spacer()
+				
+				VStack{
+					
+					LinearGradient(
+						gradient: Gradient(colors: [
+							Color(#colorLiteral(red: 0.6470588235, green: 0.9411764706, blue: 0.9843137255, alpha: 1)),
+						]),
+						startPoint: .topLeading,
+						endPoint: .bottomLeading
+					)
+					LinearGradient(
+						gradient: Gradient(colors: [
+							Color(#colorLiteral(red: 0.3803921569, green: 0.8274509804, blue: 0.7450980392, alpha: 1))
+						]),
+						startPoint: .topTrailing,
+						endPoint: .bottomTrailing
+					)
+					
+				}
+				
+			}
+			.ignoresSafeArea()
+			.blur(radius: 40)
+			
+			VStack{
+			
+					LinearGradient(
+						gradient: Gradient(colors: [
+							Color(red: 0, green: 0.337254902, blue: 0.337254902),
+							//Color(red: 0.368627451, green: 0.6509803922, blue: 0.8078431373)
+						]),
+						startPoint: .topLeading,
+						endPoint: .bottomLeading
+					)
+					LinearGradient(
+						gradient: Gradient(colors: [
+							Color(red: 0.368627451, green: 0.6509803922, blue: 0.8078431373),
+							//Color(red: 0.6470588235, green: 0.9411764706, blue: 0.9843137255),
+							//Color(red: 0.3803921569, green: 0.8274509804, blue: 0.7450980392)
+						]),
+						startPoint: .topTrailing,
+						endPoint: .bottomTrailing
+					)
+				
+			}
+			.ignoresSafeArea()
+			.blur(radius: 40)
+			
+			
+			RadialGradient(
+				gradient: Gradient(colors: [
+					//Color.black,
+					.clear,
+					Color.black
+				]),
+				center: UnitPoint(x: 0.5, y: 0.3),
+				startRadius: 0,
+				endRadius: 300
+			)
+			.ignoresSafeArea()
+			
+			VStack (alignment: .center){
+				Spacer()
+				Button(action: { viewModel.startOnboarding() }) {
+					Text("Start")
+				}
+				.padding()
+			}
+			// Your content here
 		}
+		
+		
 	}
 }
 
