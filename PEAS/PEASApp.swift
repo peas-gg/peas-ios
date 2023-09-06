3148c843cf7b1b427e74d682b4a0958068ab1249
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-09-05 18:21:38 -0700

    Force app to light mode the way that works

--- PEAS/PEASApp.swift
+++ PEAS/PEASApp.swift
@@ -12,7 +12,11 @@ struct PEASApp: App {
 	var body: some Scene {
 		WindowGroup {
 			AppView()
-				.environment(\.colorScheme, .light)
+				.onAppear {
+					if let keyWindow = UIApplication.shared.connectedScenes.compactMap({ $0 as? UIWindowScene }).flatMap({ $0.windows }).first(where: { $0.isKeyWindow }) {
+						keyWindow.overrideUserInterfaceStyle = .light
+					}
+				}
 		}
 	}
 }
