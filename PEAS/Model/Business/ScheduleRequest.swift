df2c9968861b0f93af660d94e8bc775e8ae9d194
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-09-19 19:12:20 -0700

    Add Schedule request

---
+++ PEAS/Model/Business/ScheduleRequest.swift
@@ -0,0 +1,14 @@
+//
+//  ScheduleRequest.swift
+//  PEAS
+//
+//  Created by Kingsley Okeke on 2023-09-19.
+//
+
+import Foundation
+
+struct ScheduleRequest {
+	let dayOfWeek: Int
+	let startTime: Date
+	let endTime: Date
+}
