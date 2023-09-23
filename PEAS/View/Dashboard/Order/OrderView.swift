3cfe10f1cc3e0e48f51173f536cfec54d2b48bd0
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-09-22 23:24:38 -0700

    Add OrderView file and fix compilation error in the CustomersView

---
+++ PEAS/View/Dashboard/Order/OrderView.swift
@@ -0,0 +1,20 @@
+//
+//  OrderView.swift
+//  PEAS
+//
+//  Created by Kingsley Okeke on 2023-09-22.
+//
+
+import SwiftUI
+
+struct OrderView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct OrderView_Previews: PreviewProvider {
+	static var previews: some View {
+		OrderView()
+	}
+}
