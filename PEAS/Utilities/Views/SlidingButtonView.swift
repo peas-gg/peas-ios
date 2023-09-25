0912a1af880f2b3f3d5fadbbdd89b81e41b8b917
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-09-24 20:41:03 -0700

    Update the SlidingButtonView to match the style for the CashOutView

--- PEAS/Utilities/Views/SlidingButtonView.swift
+++ PEAS/Utilities/Views/SlidingButtonView.swift
@@ -8,41 +8,72 @@
 import SwiftUI
 
 struct SlidingButtonView: View {
-	let height: CGFloat = 60
+	let height: CGFloat = 70
+	let baseOffset: CGFloat = 6
+	let buttonDimension: CGFloat = 60
 	
-	@State var xOffset: CGFloat = 6
+	@State var xOffset: CGFloat
+	@State var width: CGFloat = .zero
+	
+	@State var didComplete: Bool = false
+	
+	var maxXDistance: CGFloat {
+		width - buttonDimension - baseOffset
+	}
+	
+	//Clients
+	let feedbackClient: FeedbackClient = FeedbackClient.shared
+	
+	init() {
+		self._xOffset = State(initialValue: baseOffset)
+	}
 	
 	var body: some View {
 		HStack {
 			GeometryReader { proxy in
 				let width: CGFloat = proxy.frame(in: .local).width
 				HStack {
-					Circle()
-						.fill(Color.red)
-						.frame(width: 50, height: 50)
-						.offset(x: xOffset)
+					ZStack {
+						Circle()
+							.fill(Color.app.accent)
+						Circle()
+							.stroke(
+								LinearGradient(
+									colors: [Color.clear, Color.white.opacity(0.5), Color.white],
+									startPoint: .leading,
+									endPoint: .trailing
+								)
+							)
+						Image(systemName: didComplete ? "checkmark" : "arrow.right")
+							.font(.system(size: 26, weight: .bold))
+							.foregroundColor(Color.white)
+					}
+					.frame(dimension: buttonDimension)
+					.offset(x: xOffset)
 				}
 				.frame(width: width, height: proxy.size.height, alignment: .leading)
+				.onAppear { self.width = width }
 				.gesture(
 					DragGesture()
 						.onChanged { value in
-							let xDistance  = value.translation.width
-							print("WIDTH: \(width) X: \(xDistance)")
-							if (6..<width - 50).contains(xDistance) {
+							if (baseOffset..<maxXDistance).contains(value.translation.width) {
 								self.xOffset = value.translation.width
 							}
 						}
 						.onEnded { _ in
-							if self.xOffset < (width - 6) {
-								self.xOffset = 6
+							if self.xOffset < (maxXDistance - baseOffset) {
+								self.xOffset = baseOffset
+							} else {
+								self.didComplete = true
+								self.feedbackClient.medium()
 							}
 						}
 				)
 			}
-			.frame(height: self.height)
+			.frame(height: height)
+			.animation(.spring(), value: xOffset)
 		}
-		.background(Color.black, in: Capsule())
-		.overlay(Capsule().stroke(Color.black, lineWidth: 2))
+		.background(Color.app.darkGreen, in: Capsule())
 	}
 }
 
