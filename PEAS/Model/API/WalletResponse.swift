c352da3d63959f1d2204ea35aa23714d81e59316
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-10-07 14:45:44 -0700

    Delete TransactionResponse file Delete WithdrwalResponse file Delete Transaction file Delete Withdrawal file
    Encapsulate Earning and Withdrawal in the Transaction model which is encapsulated inside of the Wallet model

--- PEAS/Model/API/WalletResponse.swift
+++ PEAS/Model/API/WalletResponse.swift
@@ -7,7 +7,37 @@
 
 import Foundation
 
-struct WalletResponse {
+struct WalletResponse: Decodable {
+	struct TransactionResponse: Decodable {
+		struct EarningResponse: Decodable {
+			let orderId: String
+			let title: String
+			let base: Int
+			let deposit: Int
+			let tip: Int
+			let fee: Int
+			let total: Int
+			let created: String
+			let completed: String?
+		}
+		
+		struct WithdrawalResponse: Decodable {
+			let amount: Int
+			let withdrawalStatus: Wallet.Transaction.Withdrawal.Status
+			let created: String
+			let completed: String?
+		}
+		
+		enum TransactionType: Decodable {
+			case Earning
+			case Withdrawal
+		}
+		
+		let transactionType: TransactionType
+		let earning: EarningResponse?
+		let withdrawal: WithdrawalResponse?
+	}
+	
 	let balance: Int64
 	let holdBalance: Int64
 	let transactions: [TransactionResponse]
--- PEAS/Model/Business/Wallet.swift
+++ PEAS/Model/Business/Wallet.swift
@@ -8,6 +8,84 @@
 import Foundation
 
 struct Wallet: Codable {
+	struct Transaction: Codable {
+		struct Earning: Codable {
+			let orderId: String
+			let title: String
+			let base: Int
+			let deposit: Int
+			let tip: Int
+			let fee: Int
+			let total: Int
+			let created: Date
+			let completed: Date?
+			
+			init(earningResponse: WalletResponse.TransactionResponse.EarningResponse) {
+				self.orderId = earningResponse.orderId
+				self.title = earningResponse.title
+				self.base = earningResponse.base
+				self.deposit = earningResponse.deposit
+				self.tip = earningResponse.tip
+				self.fee = earningResponse.fee
+				self.total = earningResponse.total
+				self.created = ServerDateFormatter.formatToLocal(from: earningResponse.created)
+				self.completed = {
+					if let completed = earningResponse.completed {
+						return ServerDateFormatter.formatToLocal(from: completed)
+					}
+					return nil
+				}()
+			}
+		}
+		
+		struct Withdrawal: Codable {
+			enum Status: Codable {
+				case Pending
+				case Succeeded
+				case Failed
+			}
+			
+			let amount: Int
+			let withdrawalStatus: Status
+			let created: Date
+			let completed: Date?
+			
+			init(withdrawalResponse: WalletResponse.TransactionResponse.WithdrawalResponse) {
+				self.amount = withdrawalResponse.amount
+				self.withdrawalStatus = withdrawalResponse.withdrawalStatus
+				self.created = ServerDateFormatter.formatToLocal(from: withdrawalResponse.created)
+				self.completed = {
+					if let completed = withdrawalResponse.completed {
+						return ServerDateFormatter.formatToLocal(from: completed)
+					}
+					return nil
+				}()
+			}
+		}
+		
+		enum Info: Codable {
+			case earning(Earning)
+			case withdrawal(Withdrawal)
+		}
+		
+		let info: Info
+		
+		init(transactionResponse: WalletResponse.TransactionResponse) throws {
+			switch transactionResponse.transactionType {
+			case .Earning:
+				if let earning = transactionResponse.earning {
+					self.info = .earning(Earning(earningResponse: earning))
+				}
+				throw AppError.APIClientError.decodingError
+			case .Withdrawal:
+				if let withdrawal = transactionResponse.withdrawal {
+					self.info = .withdrawal(Withdrawal(withdrawalResponse: withdrawal))
+				}
+				throw AppError.APIClientError.decodingError
+			}
+		}
+	}
+	
 	let balance: Int64
 	let holdBalance: Int64
 	let transactions: [Transaction]
@@ -16,5 +94,30 @@ struct Wallet: Codable {
 		self.balance = walletResponse.balance
 		self.holdBalance = walletResponse.holdBalance
 		self.transactions = walletResponse.transactions.compactMap({ try? Transaction(transactionResponse: $0) })
+	}
+}
+
+extension Wallet.Transaction {
+	var created: Date {
+		switch info {
+		case .earning(let earning):
+			return earning.created
+		case .withdrawal(let withdrawal):
+			return withdrawal.created
+		}
+	}
+}
+
+extension Wallet {
+	static var mock1: Self {
+		return Wallet(
+			walletResponse: WalletResponse(
+				balance: 10000,
+				holdBalance: 800,
+				transactions: [
+					
+				]
+			)
+		)
 	}
 }
