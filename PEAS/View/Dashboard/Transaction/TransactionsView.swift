3ac875513ccaeac31c2a4a55be4f44af20beeabf
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-10-07 17:34:05 -0700

    Add the details for the transactions view

--- PEAS/View/Dashboard/Transaction/TransactionsView.swift
+++ PEAS/View/Dashboard/Transaction/TransactionsView.swift
@@ -11,7 +11,110 @@ struct TransactionsView: View {
 	@StateObject var viewModel: ViewModel
 	
 	var body: some View {
-		Text("Hello, World!")
+		VStack {
+			ScrollView {
+				LazyVStack {
+					ForEach(viewModel.transactions.indices, id: \.self) { index in
+						transactionView(viewModel.transactions[index])
+					}
+				}
+				.padding(.horizontal)
+			}
+		}
+		.background(Color.app.secondaryBackground)
+	}
+	
+	@ViewBuilder
+	func transactionView(_ transaction: Wallet.Transaction) -> some View {
+		HStack {
+			VStack(alignment: .leading, spacing: 4) {
+				title(transaction: transaction)
+				subTitle(transaction: transaction)
+			}
+			Spacer(minLength: 0)
+			amount(transaction: transaction)
+		}
+	}
+	
+	@ViewBuilder
+	func title(transaction: Wallet.Transaction) -> some View {
+		let content: String = {
+			switch transaction.info {
+			case .earning(let earning):
+				return earning.title
+			case .withdrawal:
+				return "CashOut"
+			}
+		}()
+		Text(content)
+			.font(Font.app.bodySemiBold)
+			.foregroundStyle(Color.app.primaryText)
+			.lineLimit(1)
+	}
+	
+	@ViewBuilder
+	func subTitle(transaction: Wallet.Transaction) -> some View {
+		let content: String = {
+			switch transaction.info {
+			case .earning(let earning):
+				return "#\(String(earning.orderId.prefix(5)))"
+			case .withdrawal(let withdrawal):
+				return "CashOut \(withdrawal.withdrawalStatus)"
+			}
+		}()
+		Text(content)
+			.font(Font.app.body)
+			.foregroundStyle(Color.app.tertiaryText)
+			.lineLimit(1)
+	}
+	
+	@ViewBuilder
+	func amount(transaction: Wallet.Transaction) -> some View {
+		let amount: Int = {
+			switch transaction.info {
+			case .earning(let earning):
+				return earning.total
+			case .withdrawal(let withdrawal):
+				return withdrawal.amount
+			}
+		}()
+		
+		let amountSign: String = {
+			switch transaction.info {
+			case .earning:
+				return ""
+			case .withdrawal:
+				return "-"
+			}
+		}()
+		
+		let amountString: String = "\(amountSign)$\(PriceFormatter.price(value: String(amount)))"
+		
+		let foregroundColor: Color = {
+			switch transaction.info {
+			case .earning:
+				return Color.app.approvedText
+			case .withdrawal:
+				return Color.app.completedText
+			}
+		}()
+		
+		let backgroundColor: Color = {
+			switch transaction.info {
+			case .earning:
+				return Color.app.approvedBackground
+			case .withdrawal:
+				return Color.app.completedBackground
+			}
+		}()
+		
+		Text(amountString)
+			.font(Font.app.body)
+			.foregroundStyle(foregroundColor)
+			.padding(4)
+			.padding(.horizontal, 6)
+			.background(backgroundColor)
+			.cornerRadius(5)
 	}
 }
 
