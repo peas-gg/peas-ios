02e8cb3e66dabde67955f7d6c3da7177e517de35
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2023-12-29 03:05:09 -0600

    Add the DateTimePicker for dates and times

---
+++ PEAS/Utilities/Views/DateTimePicker.swift
@@ -0,0 +1,70 @@
+//
+//  DateTimePicker.swift
+//  PEAS
+//
+//  Created by Kingsley Okeke on 2023-12-29.
+//
+
+import SwiftUI
+
+struct DateTimePicker: View {
+	enum Context {
+		case day
+		case time
+		case dayAndTime
+	}
+	
+	let context: Context
+	
+	@Binding var date: Date
+	
+	var body: some View {
+		switch context {
+		case .day:
+			HStack {
+				Image(systemName: "calendar")
+					.font(.system(size: FontSizes.title2, weight: .regular, design: .rounded))
+					.foregroundStyle(Color.app.tertiaryText)
+				Spacer(minLength: 0)
+				Text("\(weekDay(style: .wide))")
+				Spacer(minLength: 0)
+				Text("\(date.dayOfMonth)")
+				Spacer(minLength: 0)
+				Text("\(month())")
+				Spacer(minLength: 0)
+				Text("\(year())")
+				Spacer(minLength: 0)
+				Image(systemName: "chevron.down")
+					.foregroundColor(Color.app.tertiaryText)
+			}
+			.font(Font.app.bodySemiBold)
+			.padding()
+			.background(CardBackground())
+		case .time:
+			EmptyView()
+		case .dayAndTime:
+			EmptyView()
+		}
+	}
+	
+	func weekDay(style: Date.FormatStyle.Symbol.Weekday) -> String {
+		return self.date.formatted(Date.FormatStyle().weekday(style))
+	}
+	
+	func month() -> String {
+		return self.date.formatted(Date.FormatStyle().month(.abbreviated))
+	}
+	
+	func year() -> String {
+		return self.date.formatted(Date.FormatStyle().year(.extended()))
+	}
+}
+
+#Preview("Day") {
+	VStack {
+		Spacer()
+		DateTimePicker(context: .day, date: Binding.constant(Date()))
+			.padding(.horizontal)
+		Spacer()
+	}
+}
