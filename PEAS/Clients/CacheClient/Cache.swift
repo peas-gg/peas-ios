c04e619fb92c96c4519e55a53b5fc41b93a92e79
Kingsley Okeke <Kingsleyokeke14@gmail.com>
2024-01-08 15:19:08 -0600

    Add TimeBlocks to the Cache and add a  TimeBlockRepository

--- PEAS/Clients/CacheClient/Cache.swift
+++ PEAS/Clients/CacheClient/Cache.swift
@@ -24,5 +24,6 @@ extension CacheKey {
 	static var businessDraft: CacheKey<Business> { .init(name: "businessDraft") }
 	static var customers: CacheKey<IdentifiedArrayOf<Customer>> { .init(name: "customers") }
 	static var orders: CacheKey<IdentifiedArrayOf<Order>> { .init(name: "orders") }
+	static var timeBlocks: CacheKey<IdentifiedArrayOf<TimeBlock>> { .init(name: "timeBlocks") }
 	static var wallet: CacheKey<Wallet> { .init(name: "wallet") }
 }
--- PEAS/Model/Business/TimeBlock.swift
+++ PEAS/Model/Business/TimeBlock.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct TimeBlock: Decodable, Identifiable {
+struct TimeBlock: Codable, Identifiable {
 	let id: String
 	let title: String
 	let startTimeDate: Date
